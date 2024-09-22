1b8894762bc97318ec9f1c896aaac641abe0046a
Asad Aslam <opworrier@gmail.com>
2024-09-23 00:52:22 +0500

    updation

--- main.js
+++ main.js
@@ -13,3 +13,54 @@ navLink.forEach(link => {
         hamburger.classList.toggle('ri-close-large-line')
     })
 })
+// change background header
+const scrollHeader = () => {
+    const header = document.getElementById("navbar")
+
+    if (this.scrollY >= 50) {
+        header.classList.add("border-b", "border-[#64F3AA]")
+    } else {
+        header.classList.remove("border-b", "border-[#64F3AA]")
+    }
+}
+window.addEventListener("scroll", scrollHeader)
+
+// scrollup arrow button
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up")
+
+    if (this.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2")
+        scrollUpBtn.classList.remove("bottom-4")
+    }
+}
+window.addEventListener("scroll", scrollUp)
+
+// Scroll sections active link
+const activeLink = () => {
+    const section = document.querySelectorAll('section')
+    const navlinks = document.querySelectorAll(".nav-link")
+
+    let current = "home"
+
+    section.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id")
+        }
+    })
+
+    navlinks.forEach(item => {
+        item.classList.remove("active")
+        if (item.href.includes(current)) {
+            item.classList.remove("active")
+
+        }
+    })
+}
+
+window.addEventListener("scroll", activeLink)
